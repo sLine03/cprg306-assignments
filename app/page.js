ce01658991a0e6332589dd3916b085df3e922e61
sLine03 <celine.thai@edu.sait.ca>
2025-09-09 14:06:44 -0600

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Home(){
+
+  return(
+  <main>
+    <h1>CPRG306 Assignments</h1>
+    <ul>
+      <li><Link href="./week-2">Week 2 Assignment</Link></li>
+    </ul>
+  </main>
+
+
+  );
+}
