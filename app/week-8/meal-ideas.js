87477232780e8ecf8a23a39097bc5e60c86281c9
sLine03 <celine.thai@edu.sait.ca>
2025-12-14 21:44:15 -0700

    Create meal-ideas.js

---
+++ app/week-8/meal-ideas.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export default function MealIdeas(){
+    const [ meal, setMeal] = useState();
+    
+
+    return(
+        <main>
+            <h1>
+                <p>
+
+                </p>
+            </h1>
+        </main>
+    )
+
+}
